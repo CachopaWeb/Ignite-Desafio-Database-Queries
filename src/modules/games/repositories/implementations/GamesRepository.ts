2dc67666af58b382b096b03be27f2caf5f03f30c
Alessandro Dutra de Andrade <cachopaweb@gmail.com>
2022-04-25 16:40:39 -0400

    finnished challenge

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,26 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
+    return await this.repository
       .createQueryBuilder()
-      // Complete usando query builder
+      .andWhere('LOWER(title) LIKE :title', { title: `%${param.toLowerCase()}%` })
+      .getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT COUNT(*) FROM games'); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    return await this.repository
+      .createQueryBuilder('games')
+      .leftJoin('games.users', 'users')
+      .select([
+        'first_name',
+        'last_name',
+        'email'
+      ])
+      .where('games.id = :id', { id: id })
+      .getRawMany();
   }
 }
